da8c173aa52ab38230c9852f4dcbe900ae2d4439
y44k0v <17897299+y44k0v@users.noreply.github.com>
2026-05-06 22:56:10 -0400

    Update settings.py bottom

--- semmelweis_project/settings.py
+++ semmelweis_project/settings.py
@@ -125,10 +125,10 @@ DEFAULT_AUTO_FIELD = 'django.db.models.BigAutoField'
 
 
 STATIC_URL = "/static/"
+
+
 STATICFILES_DIRS = [BASE_DIR / "static"]
 
 MEDIA_URL = "/media/"
 MEDIA_ROOT = BASE_DIR / "media"
 
-# Store uploaded CSV records in the DB
-DEFAULT_AUTO_FIELD = "django.db.models.BigAutoField"
